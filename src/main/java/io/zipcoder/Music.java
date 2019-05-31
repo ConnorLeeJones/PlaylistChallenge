53d13fcedf3a31bd914ea99dcc4ad8f93b05c839
Connor Jones <ConnorLeeJones@gmail.com>
2019-05-31 15:14:46 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,38 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class Music {
 
     private String[] playList;
+    private int minClicks;
+    ArrayList<Integer> targets = new ArrayList<Integer>();
+
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
+
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        minClicks = Arrays.asList(playList).indexOf(selection) - startIndex;
+
+        for(int i = 0; i < playList.length; i++){
+            if (playList[i].equals(selection)){
+                targets.add(i);
+            }
+        }
+
+
+        for (Integer target : targets){
+            if ((playList.length - target + startIndex) < minClicks){
+                minClicks = playList.length - target + startIndex;
+            }
+        }
+
+        return minClicks;
     }
 }
